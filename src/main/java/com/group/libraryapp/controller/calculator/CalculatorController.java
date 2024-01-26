eba33706ac1ccac6edb80ec6346f2b89314c9b83
CapitalSnob <sorkejso@gmail.com>
2024-01-26 22:46:17 +0900

    6강 POST학습

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,27 +1,23 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //api의 진입 지점 설정
 public class CalculatorController {
-/*
-import org.springframework.web.bind.annotation.RequestParam;
 
-    @GetMapping("/add") // Http Method : GET
-    public int addTwoNumbers(
-            @RequestParam int number1, //@RequestParam: 주어지는 쿼리를 함수 파라미터에 넣는다.
-            @RequestParam int number2
-    ) {
-        return number1 + number2;
-    }
-*/
-
-    //DTO 사용 방식
     @GetMapping("/add")
-    public int addTwoNumbers(CalculatorAddRequest request) { //@RequestParam 없이 메소드로도 받아올 수 있다. 직접 받아오는 방식?
-        //CalculatorAddRequest에 Param값이 전달되는 데, 이걸 DTO(Data Transfer Object)라고 부른다.
+    public int addTwoNumbers(CalculatorAddRequest request) {
         return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply") //POST /multiply
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) { //@RequestBody : Post에 대한 JSON body를 request로 줄 수 있음.
+        return request.getNumber1() * request.getNumber2();
+
     }
 }
