2484c71bfb99631832e21814f16719eb0745cf09
CapitalSnob <sorkejso@gmail.com>
2024-01-26 23:39:26 +0900

    7강 User 조회 API 형성 - GET /user add - UserResponse .java add - UserResponse에 대하여, name과 age에 대한 값을 User로부터 가져오는 것으로 바꿈.  - UserController에서 users에서 post값을 받고, 이 users를 get에 대하여 값을 가져옴.  - @RestController에 의해, getter를 가진 객체 반환 시 JSON을 반환하게 됨. *

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,6 +2,8 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.response.UesrResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,16 @@ public class UserController {
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
         users.add(new User(request.getName(), request.getAge())); // 해당 리스트에 추가
+    }
+
+    @GetMapping("/user")
+    public List<UesrResponse> getUsers() {
+        List<UesrResponse> responses = new ArrayList<>();
+
+        for (int i = 0; i< users.size(); i++) {
+            responses.add(new UesrResponse(i + 1, users.get(i)));
+        }
+        return responses;
     }
 
 }
