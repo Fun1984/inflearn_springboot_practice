1393a66153076f2f4b03ab722dab019b3b13119c
CapitalSnob <sorkejso@gmail.com>
2024-01-27 17:23:54 +0900

    13강 Spring DB 사용하기 - 전체적인 UserController 수정, UserResponse에 생성자 추가 - jdbcTemplate를 통하여 mysql로 데이터 저장 - mysql 설치 시, 사용자명과 비밀번호를 기억해둘 것. - 데이터를 불러오는 방법(GET)은 두 가지가 있음. mapRow를 활용하거나 람다식을 통하여 간략하게 전환시키거나 #람다식에 대해서는 코틀린 입무 강의를 참고해볼 것.

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,34 +1,58 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
-import com.group.libraryapp.dto.user.request.response.UesrResponse;
+import com.group.libraryapp.dto.user.request.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>(); //리스트 생성. User의 정보들을 담음. ArrayList로써.
+    private final JdbcTemplate jdbcTemplate; //jdbc 탬플릿 선언
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }//생성자를 만들지 않아도 스프링이 jdbc템플릿 안에 정보를 넣어준다.
 
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge())); // 해당 리스트에 추가
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)"; // ?는 문자열 변수를 의미
+        jdbcTemplate.update(sql, request.getName(), request.getAge()); //sql을 문장으로 쓰고, 첫번째 ?에는 getName, 두번째 ?에는 getAge를 넣어준다.
     }
 
     @GetMapping("/user")
-    public List<UesrResponse> getUsers() {
-        List<UesrResponse> responses = new ArrayList<>();
+    public List<UserResponse> getUsers() {
+        String sql = "SELECT * FROM user";
+        /*
+        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+            //Crtl + O 를 눌러준다. -> MapRow를 오버라이드 해준다.
+            // map.row는 유저 정보를 선언한 타입인 userResponse로 바꿔준다.
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+                long id = rs.getLong("id");
+                String name = rs.getString("name"); //ResultSet인 rs에서 string 타입의 name을 가져온다.
+                int age = rs.getInt("age");
+                return new UserResponse(id,name,age);
+            }
+        });
+        */
 
-        for (int i = 0; i< users.size(); i++) {
-            responses.add(new UesrResponse(i + 1, users.get(i)));
-        }
-        return responses;
+        //람다식 - RowMapper 에다가 람다식 전환
+        // 람다를 잘모른다면 자발개발자의 코틀린 입문 강의의 람다식 미리보기 강의 확인해볼 것(20분짜리)
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id,name,age);
+        });
     }
 
 }
--- src/main/java/com/group/libraryapp/dto/user/request/response/UserResponse.java
+++ src/main/java/com/group/libraryapp/dto/user/request/response/UserResponse.java
@@ -2,12 +2,19 @@ package com.group.libraryapp.dto.user.request.response;
 
 import com.group.libraryapp.domain.user.User;
 
-public class UesrResponse {
+public class UserResponse {
     private long id;
     private String name;
     private Integer age;
 
-    public UesrResponse(long id, User user) {
+    //mapRow 쪽에서의 생성자가 요구된다.
+    public UserResponse(long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public UserResponse(long id, User user) {
         this.id = id;
         this.name = user.getName();
         this.age = user.getAge();
