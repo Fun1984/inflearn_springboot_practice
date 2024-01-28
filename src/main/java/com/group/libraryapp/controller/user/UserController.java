9573764ff808eea989f2318a48b3a3b5a7384455
CapitalSnob <sorkejso@gmail.com>
2024-01-28 16:27:05 +0900

    18강 Controller의 clean code - UserController에 있던 기능들을 Service, Repository로 분리 - DB 데이터의 전달순을 Repository -> Service -> Controller 순으로 바꿈. - CRUD에 맞게 코드 재배치
    +19강 스프링빈 활용
    - UserController 외에도 UserService와 Respository에 주석을 통하여, 스프링빈 등록
    - spring 프레임워크 내 스프링빈으로 등록된 jdbc를 여러 파일 간 거치지않고, Repository에 직접 전달.

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,64 +3,38 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.request.response.UserResponse;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final JdbcTemplate jdbcTemplate; //jdbc 탬플릿 선언
+    private final UserService userService;
 
-    public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }//생성자를 만들지 않아도 스프링이 jdbc템플릿 안에 정보를 넣어준다.
 
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge());
+        userService.saveUser(request);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        String sql = "SELECT * FROM user";
-
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id,name,age);
-        });
+        return userService.getUsers();
     }
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request) {
-        String readSql = "SELECT * FROM user WHERE id = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty(); //찾는 게 있다면, 리스트에 0이 담기지만(true), 아닐 시 null이 된다.
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "UPDATE user SET name = ? WHERE id = ?";
-
-        jdbcTemplate.update(sql, request.getName(), request.getId());
-
+        userService.updateUser(request);
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name) {
-        String readSql = "SELECT * FROM user WHERE name = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty(); //찾는 게 있다면, 리스트에 0이 담기지만(true), 아닐 시 null이 된다.
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "DELETE FROM user WHERE name = ?";
-        jdbcTemplate.update(sql, name);
+        userService.deleteUser(name);
     }
 }
---
+++ src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -0,0 +1,43 @@
+package com.group.libraryapp.service.user;
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.request.response.UserResponse;
+import com.group.libraryapp.repository.user.UserRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) { //굳이 jdbc를 가져다 줄 필요가 없어짐.
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(UserCreateRequest request) {
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    public List<UserResponse> getUsers() {
+        return userRepository.getUsers();
+    }
+
+    public void updateUser(UserUpdateRequest request) {
+        if (userRepository.isUserNotExist(request.getId())) {
+            throw new IllegalArgumentException();
+        }
+        userRepository.updateUserName(request.getName(), request.getId());
+    }
+
+    public void deleteUser(String name) {
+        if (userRepository.isUserNotExist(name)) {
+            throw new IllegalArgumentException();
+        }
+        userRepository.deleteUser(name);
+    }
+}
