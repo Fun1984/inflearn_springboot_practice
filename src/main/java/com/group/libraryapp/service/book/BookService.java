77c561bd1125e1a9398785a8df406482090c7bec
CapitalSnob <sorkejso@gmail.com>
2024-01-28 17:04:59 +0900

    20강 - 스프링 컨테이너의 활용 이유 - IoC와 DI에 대한 학습.  - IoC는 스프링빈을 통하여, 스프링컨테이너에서 어떤 클래스를 쓸 지 선택하게끔 만드는 것  - DI는 선택한 것을 통하여, 컨테이너가 의존성을 넣어주게 하는 것(쓰게끔 만드는 것) - @Primary를 통한 의존성 주입 선택(중복된 종류의 스프링빈에 대한 우선권 부여)

--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -1,12 +1,20 @@
 package com.group.libraryapp.service.book;
 
 import com.group.libraryapp.repository.book.BookMemoryRepository;
+import com.group.libraryapp.repository.book.BookMySqlRepository;
 import com.group.libraryapp.repository.book.BookRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
 
-    private final BookRepository bookRepository = new BookMemoryRepository();
+    private final BookRepository bookRepository;
 
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // 인스턴스화든, 추상화든 결국은 한 번 수정하게 되면은 파일갯수만큼 증가한다. 이를 방지하기 위한 것이 스프링 컨테이너임.
     public void saveBook() {
         bookRepository.saveBook();
     }
