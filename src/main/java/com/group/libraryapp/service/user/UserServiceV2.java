d801d62dedc00e75d24d486e545ff47f13b8ca85
CapitalSnob <sorkejso@gmail.com>
2024-01-30 00:09:37 +0900

    27-28강 - 트랜잭션의 정의와 작동 - @Transactional 을 통하여, 메소드에 대한 start transaction;과 SQL 반영/미반영 설정. - IOException과 같은 Checked Exception의 경우, rollback 발생 안함. - 영속성 Context를 갖고 있는 데, 이를 통해 변경감지, 쓰기지연, 1차 캐싱을 수행함.  - 변경감지 : 명시된 save가 없어도, 자동변경 감지하여 저장  - 쓰기 지연 : 바로바로 통신하여 SQL 작업을 하는 게 아니라, 한꺼번에 저장했다가 작업 수행함.   - 통신 비용을 줄인다. (3회 할 걸, 1회만)  - 1차 캐싱 : ID 기준으로 객체를 기억함. 중복된 객체에 대한 추가 통신을 방지함.   - 해당 객체는 복제되며, 주소마저 동일하다.

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.request.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,10 +19,15 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    //아래 함수가 시작 시, start transaction;을 시작함.
+    // 함수가 예외없이 잘 끝났다면 commit
+    // 혹시라도 문제가 있을 시 rollback
+    @Transactional
     public void saveUser(UserCreateRequest request) {
-        User u = userRepository.save(new User(request.getName(), request.getAge()));
+        userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream() //findAll = select * from user
 //                .map(user -> new UserResponse(user.getId(), user.getName(), user.getAge()))
@@ -29,6 +35,7 @@ public class UserServiceV2 {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         // select * from user where id = ?'
         //Optional<User>
@@ -36,9 +43,11 @@ public class UserServiceV2 {
                 .orElseThrow(IllegalArgumentException::new);
 
         user.updateName(request.getName()); //객체를 업데이트하고
-        userRepository.save(user); //세이브 메소드 호출 -> 자동으로 update sql이 날라가게 됨.
+        //userRepository.save(user); //세이브 메소드 호출 -> 자동으로 update sql이 날라가게 됨.
+        // save가 없어도, 영속성컨텍스트로 인하여 변경감지로 자동저장됨.
     }
 
+    @Transactional
     public void deleteUser(String name) {
 
         // SELECT * FROM user where name = ?
